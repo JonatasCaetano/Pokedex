97646cb025191c794107699b06a6ff37f6c9f1b8
Jonatas Caetano <jonatas.calves@gmail.com>
2022-09-16 16:39:32 -0300

    added pokemon list retrieved from api

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,30 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pokedex/models/helpers/pokemon_types_list.dart';
 import 'package:pokedex/views/widgets/card_pokemon.dart';
 import 'package:pokedex/views/widgets/card_search.dart';
 import 'package:pokedex/views/widgets/type_button.dart';
 
-// ignore: must_be_immutable
+import '../../controllers/main_app_screen_controller.dart';
+
 class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
-
-  List types = [
-    'Fogo',
-    'Normal',
-    'Ar',
-    'Terra',
-    'Pedra',
-  ];
-  List colors = [
-    const Color(0xfff1afb2),
-    const Color(0xff49d0b0),
-    const Color(0xff9e81a9),
-    const Color(0xff2e7885),
-    const Color(0xff383332)
-  ];
-
+  const HomeScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final mainAppScreenController = Get.find<MainAppScreenController>();
     return Stack(
       children: [
         Scaffold(
@@ -68,15 +56,15 @@ class HomeScreen extends StatelessWidget {
                   child: ListView.builder(
                     padding: const EdgeInsets.only(left: 30.0),
                     scrollDirection: Axis.horizontal,
-                    itemCount: types.length,
+                    itemCount: PokemonTypesList.types.length,
                     itemBuilder: ((context, index) => Padding(
                           padding: const EdgeInsets.only(right: 8.37),
                           child: SizedBox(
                             height: 24.0,
-                            width: 66.95,
+                            //width: 66.95,
                             child: TypeButton(
-                              type: types[index],
-                              color: colors[index],
+                              type: PokemonTypesList.types[index],
+                              color: PokemonTypesList.colors[index],
                               fontSize: 14.0,
                               radius: 5.0,
                             ),
@@ -87,55 +75,77 @@ class HomeScreen extends StatelessWidget {
                 //End buttons list
 
                 //End type block
-
-                const SizedBox(
-                  height: 32.0,
+                Obx(
+                  () => mainAppScreenController.loadMostWanted.value
+                      ? SizedBox(
+                          height: 300,
+                          width: MediaQuery.of(context).size.width,
+                          child:
+                              const Center(child: CircularProgressIndicator()),
+                        )
+                      : Container(),
                 ),
 
-                /**Start most wanted block */
+                Obx(
+                  () => mainAppScreenController.mostWantedPokemon.isEmpty
+                      ? Container()
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 32.0,
+                            ),
 
-                Padding(
-                  padding: const EdgeInsets.only(left: 30.0),
-                  child: Text(
-                    'Mais procurados',
-                    textAlign: TextAlign.left,
-                    style: GoogleFonts.nunito(
-                      textStyle: const TextStyle(
-                        color: Color(0xff2f3e77),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
+                            /**Start most wanted block */
+
+                            Padding(
+                              padding: const EdgeInsets.only(left: 30.0),
+                              child: Text(
+                                'Mais procurados',
+                                textAlign: TextAlign.left,
+                                style: GoogleFonts.nunito(
+                                  textStyle: const TextStyle(
+                                    color: Color(0xff2f3e77),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+
+                            /**GridView CardPokemon */
+                            GridView.builder(
+                              padding: MediaQuery.of(context).size.width >= 375
+                                  ? const EdgeInsets.only(left: 30.0)
+                                  : const EdgeInsets.only(left: 25.0),
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount:
+                                  mainAppScreenController.mostWantedPokemon.length,
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount:
+                                    MediaQuery.of(context).size.width ~/ 159.63,
+                                childAspectRatio: 159.53 / 110.94,
+                                crossAxisSpacing: 7,
+                                mainAxisSpacing: 11,
+                              ),
+                              itemBuilder: (context, index) {
+                                return CardPokemon(
+                                  pokemon: mainAppScreenController
+                                      .mostWantedPokemon[index],
+                                );
+                              },
+                            ),
+
+                            //End most wanted block
+                          ],
+                        ),
                 ),
-
-                const SizedBox(
-                  height: 10.0,
-                ),
-
-                /**GridView CardPokemon */
-                GridView.builder(
-                  padding: MediaQuery.of(context).size.width >= 375
-                      ? const EdgeInsets.only(left: 30.0)
-                      : const EdgeInsets.only(left: 25.0),
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: MediaQuery.of(context).size.width ~/ 159.63,
-                    childAspectRatio: 159.53 / 110.94,
-                    crossAxisSpacing: 7,
-                    mainAxisSpacing: 11,
-                  ),
-                  itemBuilder: (context, index) {
-                    return CardPokemon(
-                      type: types[index],
-                      color: colors[index],
-                    );
-                  },
-                ),
-
-                //End most wanted block
 
                 const SizedBox(
                   height: 19.0,
