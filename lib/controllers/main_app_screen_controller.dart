0f93bb2e1919c47467e21f7941065ca36d511f9c
Jonatas Caetano <jonatas.calves@gmail.com>
2022-09-16 16:38:02 -0300

    implemented the mostWantedPokemon method in MainAppScreenController

--- lib/controllers/main_app_screen_controller.dart
+++ lib/controllers/main_app_screen_controller.dart
@@ -1,12 +1,36 @@
 import 'package:get/get.dart';
+import 'package:pokedex/repositories/dao_factory.dart';
+
+import '../models/entities/pokemon.dart';
+import '../repositories/interfaces/pokemon_repository.dart';
 
 class MainAppScreenController extends GetxController {
-  static MainAppScreenController get to => Get.find();
+  static MainAppScreenController get to => Get.find<MainAppScreenController>();
+  PokemonRepository pokemonRepository = DaoFactory.getPokemonRepository();
 
-  int indexPage = 0;
+  RxList<Pokemon> mostWantedPokemon = <Pokemon>[].obs;
+  var indexPage = 0.obs;
+  var loadMostWanted = false.obs;
+
+  MainAppScreenController() {
+    getMostWantedPokemons();
+  }
 
   void updateIndexPage({required index}) {
-    indexPage = index;
+    indexPage.value = index;
+    update();
+  }
+
+  Future<void> getMostWantedPokemons() async {
+    loadMostWanted.value = true;
+    update();
+    mostWantedPokemon.clear();
+    await pokemonRepository.getMostWantedPokemons().then((value) {
+      mostWantedPokemon.value = value.obs;
+    }).catchError((onError) {
+      throw Exception();
+    });
+    loadMostWanted.value = false;
     update();
   }
 }
