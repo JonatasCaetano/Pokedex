a45fbbb5a3a4f26d3eb919b3bd412dd909ae0c7a
Jonatas Caetano <jonatas.calves@gmail.com>
2022-09-20 17:16:14 -0300

    feat: added userIsLoggedIn method and fixes in AccountRepository

--- lib/repositories/interfaces/account_repository.dart
+++ lib/repositories/interfaces/account_repository.dart
@@ -1,9 +1,11 @@
 import 'package:pokedex/models/entities/pokemon.dart';
-import 'package:pokedex/models/entities/user.dart';
+import 'package:pokedex/models/entities/user_entity.dart';
 
 abstract class AccountRepository {
-  Future<User> createAccount({required String email, required String password});
-  Future<User> logIn({required String email, required String password});
+  Future<UserEntity> createAccount(
+      {required String name, required String email, required String password});
+  Future<UserEntity> userIsLoggedIn();
+  Future<UserEntity> logIn({required String email, required String password});
   Future<void> logOut();
   Future<void> savePokemonFavorite({required Pokemon pokemon});
   Future<void> removePokemonFavorite({required Pokemon pokemon});
