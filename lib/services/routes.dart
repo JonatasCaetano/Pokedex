a743fd719ee8078b568af70e4453088d4e79f46d
Jonatas Caetano <jonatas.calves@gmail.com>
2022-09-20 17:29:00 -0300

    feat: added new routes in routes service

--- lib/services/routes.dart
+++ lib/services/routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/enuns/screens.dart';
+import 'package:pokedex/views/screens/create_account_screen.dart';
+import 'package:pokedex/views/screens/login_screen.dart';
 import 'package:pokedex/views/screens/main_app_screen.dart';
 import 'package:pokedex/views/screens/pokemon_screen.dart';
 import 'package:pokedex/views/screens/type_pokemons_screen.dart';
@@ -13,8 +15,12 @@ class Routes {
             MaterialPageRoute(builder: ((context) => const MainAppScreen())));
         break;
       case Screens.pokemon:
-        Navigator.push(context,
-            MaterialPageRoute(builder: ((context) => PokemonScreen(pokemon: args,))));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: ((context) => PokemonScreen(
+                      pokemon: args,
+                    ))));
         break;
       case Screens.pokemonsType:
         Navigator.push(
@@ -25,6 +31,24 @@ class Routes {
                 )),
           ),
         );
+        break;
+      case Screens.login:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: ((context) => LoginScreen()),
+          ),
+        );
+
+        break;
+      case Screens.createAccount:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: ((context) => CreateAccountScreen()),
+          ),
+        );
+
         break;
       default:
     }
