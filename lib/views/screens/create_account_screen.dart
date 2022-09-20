e97599c2cd1478edccf038a790ac35ffd0a07b3d
Jonatas Caetano <jonatas.calves@gmail.com>
2022-09-20 18:33:53 -0300

    feat: added form validation on create account screen

--- lib/views/screens/create_account_screen.dart
+++ lib/views/screens/create_account_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pokedex/views/widgets/text_form_field_widget.dart';
 
 import '../../controllers/main_app_screen_controller.dart';
 
@@ -20,9 +19,54 @@ class CreateAccountScreen extends StatelessWidget {
         key: keyTextFormField,
         child: ListView(
           children: [
-            TextFormFieldWidget(label: 'Nome', controller: name),
-            TextFormFieldWidget(label: 'Email', controller: email),
-            TextFormFieldWidget(label: 'Senha', controller: password),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: name,
+                decoration: const InputDecoration(
+                  label: Text('Senha'),
+                ),
+                validator: (text) {
+                  if (text == null || text.isEmpty || text.length < 6) {
+                    return "coloque uma seu nome";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: email,
+                decoration: const InputDecoration(
+                  label: Text('Email'),
+                ),
+                validator: (text) {
+                  if (text == null || text.isEmpty || !text.contains('@')) {
+                    return "coloque um email valido";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: password,
+                decoration: const InputDecoration(
+                  label: Text('Senha'),
+                ),
+                validator: (text) {
+                  if (text == null || text.isEmpty || text.length < 6) {
+                    return "coloque uma senha pelo menos 6 caracteres";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
