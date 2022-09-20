74e7d35e9fa378c4dba94469436d92a112b9c815
Jonatas Caetano <jonatas.calves@gmail.com>
2022-09-20 17:22:43 -0300

    feat: added AccountRepository in MainAppScreenController and implemented userIsLoggedIn, createAccount, login and logOut methods

--- lib/controllers/main_app_screen_controller.dart
+++ lib/controllers/main_app_screen_controller.dart
@@ -1,18 +1,27 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:pokedex/models/entities/user_entity.dart';
 import 'package:pokedex/repositories/dao_factory.dart';
+import 'package:pokedex/repositories/interfaces/account_repository.dart';
 
 import '../models/entities/pokemon.dart';
 import '../repositories/interfaces/pokemon_repository.dart';
 
 class MainAppScreenController extends GetxController {
   static MainAppScreenController get to => Get.find<MainAppScreenController>();
-  PokemonRepository pokemonRepository = DaoFactory.getPokemonRepository();
+  final PokemonRepository _pokemonRepository =
+      DaoFactory.getPokemonRepository();
+  final AccountRepository _accountRepository =
+      DaoFactory.getAccountRepository();
 
   RxList<Pokemon> mostWantedPokemon = <Pokemon>[].obs;
   var indexPage = 0.obs;
   var loadMostWanted = false.obs;
+  UserEntity? userEntity;
 
   MainAppScreenController() {
+    userIsLoggedIn();
     getMostWantedPokemons();
   }
 
@@ -25,7 +34,7 @@ class MainAppScreenController extends GetxController {
     loadMostWanted.value = true;
     update();
     mostWantedPokemon.clear();
-    await pokemonRepository.getMostWantedPokemons().then((value) {
+    await _pokemonRepository.getMostWantedPokemons().then((value) {
       mostWantedPokemon.value = value.obs;
     }).catchError((onError) {
       throw Exception();
@@ -34,4 +43,29 @@ class MainAppScreenController extends GetxController {
     update();
   }
 
+  Future<void> userIsLoggedIn() async {
+    userEntity = await _accountRepository.userIsLoggedIn();
+    update();
+  }
+
+  Future<void> createAccount(
+      {required String name,
+      required String email,
+      required String password}) async {
+    userEntity = await _accountRepository.createAccount(
+        name: name, email: email, password: password);
+    update();
+  }
+
+  Future<void> login({required String email, required String password}) async {
+    userEntity =
+        await _accountRepository.logIn(email: email, password: password);
+    update();
+  }
+
+  Future<void> logOut() async {
+    _accountRepository.logOut();
+    userEntity = null;
+    update();
+  }
 }
